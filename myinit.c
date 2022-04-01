40da9525c298013c28744faf0fcb2e8d6325dc7a
Yegor Druzhinin <egor.druzhinin97@gmail.com>
2022-04-01 18:23:13 +0500

    Демонизировал myinit

--- myinit.c
+++ myinit.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Myinit started haha\n");
+    if (getppid() != 1)
+    {
+        signal(SIGTSTP, SIG_IGN);
+        signal(SIGTTIN, SIG_IGN);
+        signal(SIGTTOU, SIG_IGN);
+
+        if (fork() != 0)
+        {
+            exit(0);
+        }
+
+        setsid();
+    }
+
+    struct rlimit flim;
+    getrlimit(RLIMIT_NOFILE, &flim);
+
+    for (int fd = 0; fd < flim.rlim_max; fd++)
+    {
+        close(fd);
+    }
+
+    chdir("/");
 }
