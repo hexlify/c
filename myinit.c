ed3d7680bdf0caef70bed8fbf6568d07477bf09f
Yegor Druzhinin <egor.druzhinin97@gmail.com>
2022-04-06 14:08:41 +0500

    Процесс myinit обзовелся лог-файлом. Процессы берут STDIN/STDOUT из настроек Если процесс завершился с code != 0, то перед следующим запуском он уснет на минуту

--- myinit.c
+++ myinit.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <fcntl.h>
 
 // void parseprocs(char **procs, FILE *f)
 // {
@@ -102,49 +103,80 @@
 //     }
 // }
 
-#define PROGS_COUNT 3
-#define TIMEOUT 60
+#define PROGS_COUNT 4
+#define LOG_FILE "/tmp/myinit.log"
+#define DELAY 60
 
-void fork_prog(char **prog, int *spid)
+#define PERM_644 S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+#define LOGMODE O_WRONLY | O_APPEND | O_CREAT
+
+void fork_prog(char **prog, int *spid, int delay)
 {
     int pid = fork();
     if (pid == 0)
     {
+        int in = open(prog[2], O_RDONLY);
+        int out = open(prog[3], LOGMODE, PERM_644);
+        if (in == -1 || out == -1)
+        {
+            printf("%s %s (%i):\tFailed to open input/output files, errno=%i\n", prog[0], prog[1], getpid(), errno);
+            exit(1);
+        }
+
+        dup2(in, STDIN_FILENO);
+        dup2(out, STDOUT_FILENO);
+
+        close(in);
+        close(out);
+
+        if (delay)
+        {
+            sleep(DELAY);
+        }
+
         int err = execl(prog[0], prog[0], prog[1], NULL);
         if (err == -1)
         {
-            char *desc = "";
-            if (errno == ENOENT)
-            {
-                desc = "(Executable file not found)";
-            }
-            printf("Failed to exec %s, errno=%i %s\n", prog[0], errno, desc);
+            printf("%s %s (%i):\tFailed to exec, errno=%i\n", prog[0], prog[1], getpid(), errno);
             exit(1);
         }
     }
     else if (pid > 0)
     {
         *spid = pid;
+        if (delay)
+        {
+            printf("%s %s (%i):\tProcess started (delayed=%i seconds)\n", prog[0], prog[1], pid, DELAY);
+        }
+        else
+        {
+            printf("%s %s (%i):\tProcess started\n", prog[0], prog[1], pid);
+        }
     }
     else
     {
-        printf("Failed to fork, errno=%i\n", errno);
+        printf("%s %s:\tFailed to fork, errno=%i\n", prog[0], prog[1], errno);
+        exit(1);
     }
 }
 
 int main()
 {
+    int out = open(LOG_FILE, LOGMODE, PERM_644);
+    dup2(out, STDOUT_FILENO);
+    close(out);
+
     char *progs[PROGS_COUNT][4] = {
         {"/tmp/mysleep.sh", "4s", "/tmp/in/1", "/tmp/out/1"},
-        {"/tmp/mysleep.sh", "1s", "/tmp/in/2", "/tmp/out/2"},
+        {"/tmp/mysleep.sh", "10s", "/tmp/in/2", "/tmp/out/2"},
         {"/tmp/mysleep.sh", "3s", "/tmp/in/3", "/tmp/out/3"},
-        // {"trash", "", "/tmp/in/4", "/tmp/out/4"},
+        {"trash", "1s", "/tmp/in/4", "/tmp/out/4"},
     };
     int pids[PROGS_COUNT];
 
     for (size_t i = 0; i < PROGS_COUNT; i++)
     {
-        fork_prog(progs[i], &pids[i]);
+        fork_prog(progs[i], &pids[i], 0);
     }
 
     int wstatus;
@@ -159,8 +191,10 @@ int main()
                 break;
             }
         }
-        printf("Program pid=%i exec=%s args=%s finished with code %i\n", pid, progs[pi][0], progs[pi][1], WEXITSTATUS(wstatus));
-
-        fork_prog(progs[pi], &pids[pi]);
+        int code = WEXITSTATUS(wstatus);
+        printf("%s %s (%i):\tProcess finished with code %i\n", progs[pi][0], progs[pi][1], pid, code);
+        fork_prog(progs[pi], &pids[pi], code != 0);
     }
+
+    // wait над пидами
 }
